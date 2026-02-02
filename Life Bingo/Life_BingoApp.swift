95a726ad47c68b489344a69ca2e50dd44470888b
Jiashu Li <452472803ljs@gmail.com>
2026-02-02 12:56:54 -0500

    Initial Commit

---
+++ Life Bingo/Life_BingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_BingoApp.swift
+//  Life Bingo
+//
+//  Created by Jason Li on 2026-02-02.
+//
+
+import SwiftUI
+
+@main
+struct Life_BingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
